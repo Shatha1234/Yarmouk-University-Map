ec0c7457d2059811ffc32c040cb9161fd6f809b9
Ehab Kahwati <dwairi.razan1995@gmail.com>
2018-04-14 14:07:02 +0300

    Test1

---
+++ js/config_firebase.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDG4c2nHZIMxGDIBpueylz1SGqO_Xp2Qwg",
+    authDomain: "yarmouk-university-map.firebaseapp.com",
+    databaseURL: "https://yarmouk-university-map.firebaseio.com",
+    projectId: "yarmouk-university-map",
+    storageBucket: "yarmouk-university-map.appspot.com",
+    messagingSenderId: "573319191440"
+  };
+  firebase.initializeApp(config);
